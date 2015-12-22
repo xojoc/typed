9e5695761a60b1e6dc7e68320522380887969826
Alexandru Cojocaru <xojoc@gmx.com>
2015-12-22 01:44:04 +0100

    s

--- main.go
+++ main.go
@@ -189,10 +189,10 @@ func aHandler(w http.ResponseWriter, r *http.Request) *NetError {
 	w.Header().Add("Cache-Control", "no-cache")
 	w.Header().Add("ETag", fmt.Sprint(a.ETag))
 	if r.Header.Get("If-None-Match") == fmt.Sprint(a.ETag) {
-		fmt.Println("equal")
+		log.Println("equal")
 		return &NetError{304, ""}
 	} else {
-		fmt.Println("not equal ", r.Header.Get("If-None-Match"), fmt.Sprint(a.ETag))
+		log.Println("not equal ", r.Header.Get("If-None-Match"), fmt.Sprint(a.ETag))
 	}
 	err = templates.ExecuteTemplate(w, "a.html", a)
 	if err != nil {
